a120570718680490bd4c4f647c39e197e8aa8441
xavpaw <159389023+xavpaw@users.noreply.github.com>
2024-02-14 09:23:15 +1300

    Basic instructions and simple yes/no function

---
+++ roll it game/C_01_yes_no.py
@@ -0,0 +1,20 @@
+# checks users enter yes (y) or no (n)
+def yes_no(question):
+    while True:
+        response = input(question).lower()
+
+        if response == "yes" or response == "y":
+            return "yes"
+        elif response == "no" or response == "n":
+            return "no"
+        else:
+            print("You did not choose a valid response")
+
+
+#Main routine
+while True:
+    want_instructions = yes_no("Do you want to read the instructions? ")
+    print(f"You chose {want_instructions}")
+
+    roll_again = yes_no("Do you want to roll again? ")
+    print(f"You said {roll_again} to rolling again. ")
